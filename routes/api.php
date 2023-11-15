d449824f49b8755fb62a8a19cfbda598bfa03b22
sam <boadusamuel@gmail.com>
2023-11-15 17:31:17 +0000

    added authentication and authorization

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\InvoiceController;
 use App\Http\Controllers\ItemController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,27 +23,34 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::prefix('v1.0')->group(function () {
-    Route::prefix('items')->group(function () {
-        Route::get('/', [ItemController::class, 'index']);
-        Route::post('/', [ItemController::class, 'store']);
-        Route::put('/{item}', [ItemController::class, 'update']);
-        Route::get('/{item}', [ItemController::class, 'show']);
-        Route::put('{item}/increment-quantity', [ItemController::class, 'addItemQuantity']);
-    });
 
-    Route::prefix('customers')->group(function () {
-        Route::get('/', [CustomerController::class, 'index']);
-        Route::post('/', [CustomerController::class, 'store']);
-        Route::get('/{customer}', [CustomerController::class, 'show']);
-    });
+    Route::post('/login', [UserController::class, 'login']);
 
-    Route::prefix('invoices')->group(function () {
-        Route::get('/', [InvoiceController::class, 'index']);
-        Route::post('/', [InvoiceController::class, 'store']);
-        Route::get('/{invoice}', [InvoiceController::class, 'show']);
-        Route::put('/{invoice}', [InvoiceController::class, 'update']);
-        Route::put('/{invoice}/items', [InvoiceController::class, 'updateInvoiceItems']);
-        Route::delete('/{invoice}', [InvoiceController::class, 'destroy']);
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::prefix('items')->group(function () {
+            Route::get('/', [ItemController::class, 'index']);
+            Route::post('/', [ItemController::class, 'store']);
+            Route::put('/{item}', [ItemController::class, 'update']);
+            Route::get('/{item}', [ItemController::class, 'show']);
+            Route::put('{item}/increment-quantity', [ItemController::class, 'addItemQuantity']);
+        });
+
+        Route::prefix('customers')->group(function () {
+            Route::get('/', [CustomerController::class, 'index']);
+            Route::post('/', [CustomerController::class, 'store']);
+            Route::get('/{customer}', [CustomerController::class, 'show']);
+        });
+
+        Route::prefix('invoices')->group(function () {
+            Route::get('/', [InvoiceController::class, 'index']);
+            Route::post('/', [InvoiceController::class, 'store']);
+            Route::get('/{invoice}', [InvoiceController::class, 'show']);
+            Route::put('/{invoice}', [InvoiceController::class, 'update']);
+            Route::put('/{invoice}/items', [InvoiceController::class, 'updateInvoiceItems']);
+            Route::delete('/{invoice}', [InvoiceController::class, 'destroy']);
+        });
+
+        Route::post('/logout', [UserController::class, 'logout']);
     });
 });
 
